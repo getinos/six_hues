76688b16e084e2a901f71c699fabf4d69b4b53fb
getinos <sujaydas0325@gmail.com>
2025-03-09 14:39:24 +0530

    issue fixed

--- 6hues-20250228T122404Z-001/6hues/six_hues/project/index.js
+++ 6hues-20250228T122404Z-001/6hues/six_hues/project/index.js
@@ -28,7 +28,7 @@ const setDiameter = () => {
     let slider = document.querySelector('.slider');
     let widthSlider = slider.offsetWidth;
     let heightSlider = slider.offsetHeight;
-    let diameter = Math.sqrt(Math.pow(widthSlider, 2) + Math.pow(heightSlider, 2));
+    let diameter = Math.sqrt(Math.pow(widthSlider, 2) + Math.pow(heightSlider, 2))+1000;
     document.documentElement.style.setProperty('--diameter', diameter + 'px');
 }
 
